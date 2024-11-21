e7837040c5fb1bed7bd679920e93d0f722497917
Busapalli Nandiswar <140873309+nandiswarb93@users.noreply.github.com>
2024-11-21 18:04:22 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -28,7 +28,7 @@ const Login = () => {
     try {
       // Send a POST request to the backend for authentication
       const response = await axios.post(
-        "http://localhost:5000/login",
+        "https://admin-task-dealsdray.onrender.com/login",
         loginData
       );
 
